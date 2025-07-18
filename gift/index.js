e7b090fea4436411db9ce5cc35aa8cca53f2b989
Gifted Sender Api <senderapi@giftedtech.web.id>
2025-07-18 19:57:04 +0300

    Update index.js

--- gift/index.js
+++ gift/index.js
@@ -2,8 +2,8 @@ const { evt, gmd, commands } = require('./gmdCmds');
 const config = require('../config');
 
 const { createContext, createContext2 } = require('./gmdHelpers');
-const { logger, GiftedListeners, GiftedAntiLink, GiftedAutoBio, GiftedChatBot, GiftedPresence, GiftedAntiDelete, GiftedAnticall } = require('./gmdFunctions2');
+const { logger, GiftedAntiLink, GiftedAutoBio, GiftedChatBot, GiftedPresence, GiftedAntiDelete, GiftedAnticall } = require('./gmdFunctions2');
 const { toAudio, toVideo, toPtt, formatVideo, formatAudio, monospace, runtime, sleep, gmdFancy, stickerToImage, formatBytes, gmdBuffer, webp2mp4File, gmdJson, latestWaVersion, gmdRandom, isUrl, gmdStore, isNumber, loadSession, verifyJidState } = require('./gmdFunctions');
 
 
-module.exports = { evt, gmd, config, commands, toAudio, toVideo, toPtt, formatVideo, formatAudio, runtime, sleep, gmdFancy, stickerToImage, monospace, formatBytes, createContext, createContext2, GiftedChatBot, GiftedAntiLink, GiftedListeners, GiftedAntiDelete, GiftedAnticall, GiftedPresence, GiftedAutoBio, logger, gmdBuffer, webp2mp4File, gmdJson, latestWaVersion, gmdRandom, isUrl, gmdStore, isNumber, loadSession, verifyJidState };
+module.exports = { evt, gmd, config, commands, toAudio, toVideo, toPtt, formatVideo, formatAudio, runtime, sleep, gmdFancy, stickerToImage, monospace, formatBytes, createContext, createContext2, GiftedChatBot, GiftedAntiLink, GiftedAntiDelete, GiftedAnticall, GiftedPresence, GiftedAutoBio, logger, gmdBuffer, webp2mp4File, gmdJson, latestWaVersion, gmdRandom, isUrl, gmdStore, isNumber, loadSession, verifyJidState };
